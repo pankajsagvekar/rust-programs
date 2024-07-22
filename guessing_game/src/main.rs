601dc8fccfe255e237cddd3d48e2d11c3fda2f1d
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-07-22 19:27:05 +0530

    checking guess and adding loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,29 +1,34 @@
-use std::{cmp::Ordering, io};
 use rand::Rng;
+use std::{cmp::Ordering, io};
 
-fn main(){
+fn main() {
     println!("Guess The Number");
 
-    let random_num = rand::thread_rng().gen_range(1..=100);  //we call the rand::thread_rng function that gives us the particular random number generator
+    let random_num = rand::thread_rng().gen_range(1..=100); //we call the rand::thread_rng function that gives us the particular random number generator
     println!("The secret number is: {random_num}");
-    
-    println!("Enter The Number: ");
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess) //we call the stdin function from the io module, which will allow us to handle user input
-        .expect("Failed to read line");
-    //could have been written as
-    //io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        println!("Enter Your Guess: ");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please Type Number"); //u32 maens 32bit integer
+        io::stdin()
+            .read_line(&mut guess) //we call the stdin function from the io module, which will allow us to handle user input
+            .expect("Failed to read line");
+        //could have been written as
+        //io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    println!("You Guessed {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please Type Number"); //u32 maens 32bit integer
 
-    //comparing the guess with random number
-    match guess.cmp(&random_num) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Greater => println!("Too Big"),
-        Ordering::Equal => println!("You Win"),
+        println!("You Guessed {guess}");
+
+        //comparing the guess with random number
+        match guess.cmp(&random_num) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            },
+        }
     }
 }
