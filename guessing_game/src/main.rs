a2912e57ddba086da81c4eaa1307ae2b0961435b
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-07-22 19:18:18 +0530

    comparing if numbe is right

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main(){
@@ -13,8 +13,17 @@ fn main(){
     io::stdin()
         .read_line(&mut guess) //we call the stdin function from the io module, which will allow us to handle user input
         .expect("Failed to read line");
-
     //could have been written as
     //io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please Type Number"); //u32 maens 32bit integer
+
     println!("You Guessed {guess}");
+
+    //comparing the guess with random number
+    match guess.cmp(&random_num) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You Win"),
+    }
 }
